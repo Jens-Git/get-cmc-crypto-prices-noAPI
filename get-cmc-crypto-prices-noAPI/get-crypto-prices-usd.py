1d2a9b7598da7c593d146025848f5e22ef98df09
Jens <jens@jensbertram.de>
2021-05-30 11:29:11 +0200

    update

--- get-cmc-crypto-prices-noAPI/get-crypto-prices-usd.py
+++ get-cmc-crypto-prices-noAPI/get-crypto-prices-usd.py
@@ -17,14 +17,28 @@ def find_price(soup):
     price = soup.find(class_='priceValue___11gHJ').get_text()
     return price
 
+def find_price_change(soup):
+    price_change = soup.find(class_='sc-1v2ivon-0').get_text()
+    return price_change
+
+def get_rank(soup):
+    rank = soup.find(class_='namePill___3p_Ii namePillPrimary___2-GWA').get_text()
+    return rank
+
+
 for x in currency:
     #print ('Parsing information for '+ x +'...')
     soup = parse_page(x)
     #print ("Finding Price for " + x + "...")
     price = find_price(soup)
+    price_change = find_price_change(soup)
     #print ('Current Price for '+ x + ' is:')
+    rank = get_rank(soup)
+
     print ('----------------')
-    print ('-----' + x + '-----')
+    print ('Currency: ' +x)
+    print ('Currently at: ' +rank)
+    print ('Price: ' +price)
+    print ('24h Change: ' +price_change)
     print ('----------------')
-    print (price)
     print ()
